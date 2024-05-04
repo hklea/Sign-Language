bba7ead13c3ea6a19a42b5d909991bf6cf404a16
hklea <khaxhiu21@epoka.edu.al>
2024-05-04 16:29:06 +0200

    Till Now
    Created my own dataset
    Hand landmark

---
+++ create_dataset.py
@@ -0,0 +1,40 @@
+import os
+import cv2
+#image storage
+DATA_DIR='./mydataset'
+# Create the dataset directory if it doesn't exist
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+#num of folders ,how many each , jpg
+num_folders=26
+dataset_size=100
+img_ext='.jpg'
+
+#set the cam to capture img
+capture=cv2.VideoCapture(0);
+for i in range (num_folders):
+    # Create a directory for the current class
+    class_dir = os.path.join(DATA_DIR, str(i))
+    if not os.path.exists(class_dir):
+        os.makedirs(class_dir)
+
+    if i > 0:
+        # Display message indicating when to change the sign
+        for _ in range(5):  # Display for 5 seconds
+            ret, frame = capture.read()
+            cv2.putText(frame, "Change sign now", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('SHOOT', frame)
+            cv2.waitKey(1000)  # Wait for 1 second
+        cv2.destroyAllWindows()   
+    print(f'Collecting data for class {i}')
+    # Start capturing frames from the camera
+    for j in range (dataset_size):
+        ret,frame = capture.read();
+        cv2.imshow('SHOOT',frame)
+        img_path = os.path.join(class_dir, f'image_{j}{img_ext}')
+        cv2.imwrite(img_path, frame)
+        cv2.waitKey(100); #wait 100ms
+ 
+
+capture.release()
+cv2.destroyAllWindows()
---
+++ preprocess_dataset.py
@@ -0,0 +1,98 @@
+# import os
+# import pickle
+
+# import mediapipe as mp
+# import cv2
+# import matplotlib.pyplot as plt
+
+
+# mp_hands = mp.solutions.hands
+# mp_drawing = mp.solutions.drawing_utils
+# mp_drawing_styles = mp.solutions.drawing_styles
+
+# hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+# DATA_DIR = './mydataset'
+
+# data = []
+# labels = []
+# for dir_ in os.listdir(DATA_DIR):
+#     for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+#         data_aux = []
+
+#         x_ = []
+#         y_ = []
+
+#         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+#         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#         results = hands.process(img_rgb)
+#         if results.multi_hand_landmarks:
+#             for hand_landmarks in results.multi_hand_landmarks:
+#                 for i in range(len(hand_landmarks.landmark)):
+#                     x = hand_landmarks.landmark[i].x
+#                     y = hand_landmarks.landmark[i].y
+
+#                     x_.append(x)
+#                     y_.append(y)
+
+#                 for i in range(len(hand_landmarks.landmark)):
+#                     x = hand_landmarks.landmark[i].x
+#                     y = hand_landmarks.landmark[i].y
+#                     data_aux.append(x - min(x_))
+#                     data_aux.append(y - min(y_))
+
+#             data.append(data_aux)
+#             labels.append(dir_)
+
+# f = open('data.pickle', 'wb')
+# pickle.dump({'data': data, 'labels': labels}, f)
+# f.close()
+
+import os
+import pickle
+import mediapipe as mp
+import cv2
+
+mp_hands = mp.solutions.hands
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+DATA_DIR = './mydataset'
+
+data_dict = {}  # Dictionary to store hand landmarks and labels
+
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
+
+        x_ = []
+        y_ = []
+
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
+
+            # Add hand landmarks and label to data dictionary
+            if dir_ not in data_dict:
+                data_dict[dir_] = []
+            data_dict[dir_].append(data_aux)
+
+# Serialize the data dictionary directly into a pickle file
+with open('data.pickle', 'wb') as f:
+    pickle.dump(data_dict, f)
